329ed026cc6648715eedbadf6c8e3a06ea7d42bd
Nathan Hand <administrator@thehiddennation.com>
2013-08-10 17:02:13 +0100

    release ready.

---
+++ public_html/loading-v1.0/config.php
@@ -0,0 +1,46 @@
+<?php
+	//The title, usually your web address but can just be something like "The fancy pants community"
+	$title_text = "";
+	//Use custom font type, lots of different custom fonts can be found here: http://www.dafont.com/
+	$custom_font = false;
+	//Custom font source (if we're using a custom font type, we need the source of that fonts location) example : http://www.yourwebsitehere.com/loading/myfont.ttf
+	$font_src = "";
+	
+	//Your steamAPI key, this is required and can be applied for here: http://steamcommunity.com/dev
+	$steam_api = "";
+	
+	//Custom serverip, the serverip address comes from Steam data. If a user's profile is set to private/friends only this field will be blank, so set a custom one.
+	$custom_serverip = false;
+	
+	//This is useful for when you want user's to check for a dns (exmaple: ttt.sngaming.org) for your server, or they have a private profile.
+	$serverip_text = "";
+	
+	//The largest visible background colour by default: #2C3539
+	$body_main = "#2C3539";
+	
+	//The gradient at the top going to the bottom. Default: #1765B5
+	$header_top_gradient = "#1765B5";
+	//The gradient at the bottom going to the top/ Default: #2096E1
+	$header_bot_gradient = "#2096E1";
+	
+	//Title colour background. Default: #1061B3
+	$title_main = "#1061B3";
+	
+	//The colour of all the font in the loadingurl. Defualt: white
+	$text_colour = "white";
+	
+	//Dropshadow colour. Default: #1589FF
+	$drop_colour = "#1589FF";
+	
+	//Community name in welcome message
+	$com_name = "";
+	
+	//Welcome message text. (LEAVE BLANK IF NOT REQUIRED)
+	$welcome_message_1 = "This is the first paragraph that can easily be edited within the config file, you can keep typing for a really long paragraph/sentence or you can just go onto the next one in a new variable.";
+	
+	//Welcome message text for a second paragraph with 5px distance between the first paragraph and the second. (LEAVE BLANK IF NOT REQUIRED)
+	$welcome_message_2 = "This is the next variable, this is the 2nd sentence for the welcome message that you can again easily edit in the config file, if this isn't required you can jsut leave the varable blank.";
+	
+	//Welcome message text for a third paragraph with 5px distance between the first paragraph and the second. (LEAVE BLANK IF NOT REQUIRED)
+	$welcome_message_3 = "";
+?>
